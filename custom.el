90f696e87607402cb3284d8ab173470a41d787ff
mpsss <mephistommm@gmail.com>
2016-08-20 20:46:41 +0800

    split custom configurations to custom.el

---
+++ custom.el
@@ -0,0 +1,21 @@
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["color-233" "color-167" "color-142" "color-214" "color-109" "color-175" "color-108" "color-223"])
+ '(company-etags-ignore-case nil)
+ '(custom-safe-themes
+   (quote
+    ("2ff99812cddf171ea59ac3b458fd79faac7f0de2bdabefb49ca97b30ef5e5060" default)))
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
