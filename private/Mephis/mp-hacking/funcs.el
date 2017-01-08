5a6c7f9da5c46514704cb29d2a4711aae57cf35f
mpsss <mephistommm@gmail.com>
2017-01-08 21:36:44 +0800

    bind self hungry delete backward to <C-h> (inline) and <C-backspace> (multiline)

--- private/Mephis/mp-hacking/funcs.el
+++ private/Mephis/mp-hacking/funcs.el
@@ -12,59 +12,74 @@
 (defun mp-hacking/better-default ()
   "This function set many default config to more better!
 Some configurations should be written at here which be unable to config
-in 'with-eval-after-load."
- )
+in 'with-eval-after-load.")
 
 (defun mp-ui/format-haskell-buffer ()
   "format haskell buffer by hindent-reformat-buffer and haskell-mode-stylish-buffer"
   (interactive)
   (hindent-reformat-buffer)
-  (haskell-mode-stylish-buffer)
-  )
+  (haskell-mode-stylish-buffer))
 
-(defun mp-ui/clear-whole-line ()
-  "Delete all chars in current line"
+(defun mp-ui/hungry-delete ()
+  "Delete all spaces before current point"
   (interactive)
-  (kill-region (progn (forward-visible-line 0) (point))
-               (progn (forward-visible-line 1) (backward-char) (point)))
-  )
+  (unless (> (char-before) 32)
+    (c-hungry-delete-backwards))
+ )
+
+(defun mp-ui/hungry-delete-current-line ()
+  "Delete all spaces before current point in current line"
+  (interactive)
+  (let ((here (point)))
+    (while (progn
+             (skip-syntax-backward " \t\r\f\v")
+             (eq (char-before) ?\\))
+      (backward-char))
+    (if (< (point) here)
+        (delete-region (point) here))
+   )
+ )
 
 (define-minor-mode self-evil-play-mode
   "Buffer-local minor mode to define self evil keyboard."
   :keymap (make-sparse-keymap))
 
 (define-globalized-minor-mode global-self-evil-play-mode
-  self-evil-play-mode (lambda () (self-evil-play-mode 1))
+  self-evil-play-mode
+  (lambda ()
+    (self-evil-play-mode 1))
   "Global minor mode to define self evil keyboard.")
 
 (global-self-evil-play-mode)
 
 (defun self-evil-play-interactive-setup ()
   (setq evil-inhibit-operator t)
-  (list (assoc-default evil-this-operator
-                       self-evil-play-operator-alist)))
+  (list (assoc-default evil-this-operator self-evil-play-operator-alist)))
 
 (defmacro self-evil-play-define-key (key-sequence func)
   "Binding evil operator with the func."
-  (let ((func-name (intern (format "self-evil-play/%s" (nth 1 func)))))
+  (let ((func-name (intern (format "self-evil-play/%s"
+                                   (nth 1 func)))))
     `(progn
-       (evil-define-command ,func-name (operation)
-         ,(format "Define %s for evil operator!" (nth 1 func))
+       (evil-define-command ,func-name
+         (operation)
+         ,(format "Define %s for evil operator!"
+                  (nth 1 func))
          (interactive (self-evil-play-interactive-setup))
          (cond
           ((eq operation (quote ,(intern (substring key-sequence 0 1))))
-           (call-interactively ,func))
-          ))
-       (evil-define-key 'operator self-evil-play-mode-map ,(substring key-sequence 1) (quote ,func-name)))
-    )
-  )
+           (call-interactively ,func))))
+       (evil-define-key 'operator
+         self-evil-play-mode-map
+         ,(substring key-sequence 1)
+         (quote ,func-name)))))
 
 (defun mp-hacking/hide-js2-checks-if-flycheck-active ()
   "This function hide the result of js2-mode checking when there is a checker for flycheck."
   (message "fuck")
   (if (flycheck-get-checker-for-buffer)
-    ;;t
-    (js2-mode-hide-warnings-and-errors)
+      ;;t
+      (js2-mode-hide-warnings-and-errors)
     ;;nil
     (js2-mode-display-warnings-and-errors)))
 
